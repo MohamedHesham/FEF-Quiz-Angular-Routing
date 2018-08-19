8118380e37893ad240e7f04ca0c832af80a4bb54
Mohamed Hesham Alma'zon <m.hesham@outlook.com>
2018-08-19 07:24:33 +0200

    Add files via upload

---
+++ app/scripts/app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name routingQuizApp
+ * @description
+ * # routingQuizApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('routingQuizApp', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider,
+    $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/instructions.html'
+      })
+      .state('redBrick', {
+        url: '/bricks/red',
+        templateUrl: 'views/bricks.html',
+        controller: 'RedBricksCtrl as brick'
+      })
+      .state('blueBrick', {
+        url: '/bricks/blue',
+        templateUrl: 'views/bricks.html',
+        controller: 'BlueBricksCtrl as brick'
+      })
+      .state('greenBrick', {
+        url: '/bricks/green',
+        templateUrl: 'views/bricks.html',
+        controller: 'GreenBricksCtrl as brick'
+      })
+      .state('cart', {
+        url: '/cart',
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl as cart'
+      })
+      .state('redBrick.cart', {
+        url: '/cart',
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl as cart'
+      })
+      .state('blueBrick.cart', {
+        url: '/cart',
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl as cart'
+      })
+      .state('greenBrick.cart', {
+        url: '/cart',
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl as cart'
+      });
+  }]);
